ac8ab27cf043c343ca784d5f318ab85ceb76b04d
Frost3896 <146024772+Frost3896@users.noreply.github.com>
2023-10-17 19:56:01 +0200

    Add docstrings

--- api.py
+++ api.py
@@ -3,34 +3,38 @@ import functools
 
 
 class Api:
+    """Interact with the Guild Wars 2 API using an API key."""
+
     def __init__(self) -> None:
-        self.api_key = None
+        """Initialize an instance of the Api class."""
+        self._api_key = None
         self._clear_cache()
 
     def _clear_cache(self) -> None:
-        # Clear the cache for methods that need to be invalidated
+        """Clear the cache for all decorated methods."""
         self.check_key.cache_clear()
         self.get_account_name.cache_clear()
         self.get_character_names.cache_clear()
         self.get_profession_name.cache_clear()
 
     def _get_endpoint_v2(self, endpoint: str):
+        """Perform a GET request to the API and return the JSON response."""
         url = f"https://api.guildwars2.com/v2/{endpoint}"
-        headers = {"Authorization": f"Bearer {self.api_key}"}
+        headers = {"Authorization": f"Bearer {self._api_key}"}
         return requests.get(url, headers=headers).json()
 
     def set_api_key(self, api_key: str) -> None:
-        if self.api_key != api_key:
-            self.api_key = api_key
+        """Set the API key and clear the cache if the key changes."""
+        if self._api_key != api_key:
+            self._api_key = api_key
             self._clear_cache()
 
     @functools.lru_cache(maxsize=None)
     def check_key(self) -> bool:
-        # check if api key is valid
+        """Check if the API key is valid and has correct permissions."""
         tokeninfo = self._get_endpoint_v2("tokeninfo")
         if tokeninfo == "Invalid access token":
             return False
-        # check if correct permissions are set
         permissions = tokeninfo["permissions"]
         if "account" not in permissions:
             return False
@@ -44,16 +48,19 @@ class Api:
 
     @functools.lru_cache(maxsize=None)
     def get_account_name(self) -> str:
+        """Get the account name associated with the API key."""
         account_name = self._get_endpoint_v2("account")
         return account_name["name"]
 
     @functools.lru_cache(maxsize=None)
     def get_character_names(self) -> list[str]:
+        """Get a list of character names associated with the API key."""
         character_names = self._get_endpoint_v2("characters")
         return character_names
 
     @functools.lru_cache(maxsize=None)
     def get_profession_name(self, character: str) -> str:
+        """Get the profession name of a character."""
         profession_name = self._get_endpoint_v2(
             f"characters/{character}/core"
         )
@@ -61,11 +68,13 @@ class Api:
 
     @functools.lru_cache(maxsize=None)
     def get_skill_name(self, skill_id: int) -> str:
+        """Get the name of a skill by its ID."""
         skill_name = self._get_endpoint_v2(f"skills/{skill_id}")
         return skill_name["name"]
 
     @functools.lru_cache(maxsize=None)
     def get_specialization_name(self, specialization_id: int) -> str:
+        """Get the name of a specialization by its ID."""
         specialization_name = self._get_endpoint_v2(
             f"specializations/{specialization_id}"
         )
@@ -73,21 +82,25 @@ class Api:
 
     @functools.lru_cache(maxsize=None)
     def get_trait_name(self, trait_id: int) -> str:
+        """Get the name of a trait by its ID."""
         trait_name = self._get_endpoint_v2(f"traits/{trait_id}")
         return trait_name["name"]
 
     @functools.lru_cache(maxsize=None)
     def get_item_stat_name(self, item_stat_id: int) -> str:
+        """Get the name of an item stat by its ID."""
         item_stat_name = self._get_endpoint_v2(f"itemstats/{item_stat_id}")
         return item_stat_name["name"]
 
     def get_build_templates(self, character: str):
+        """Get build templates for a character."""
         build_templates = self._get_endpoint_v2(
             f"characters/{character}/buildtabs?tabs=all"
         )
         return build_templates
 
     def get_equipment_templates(self, character: str):
+        """Get equipment templates for a character."""
         equipment_templates = self._get_endpoint_v2(
             f"characters/{character}/equipmenttabs?tabs=all"
         )
