9de9c7dc77eae8e33b559821c07a3e531345de96
Frost3896 <146024772+Frost3896@users.noreply.github.com>
2023-10-17 21:33:00 +0200

    Add docstrings

--- snowcrows.py
+++ snowcrows.py
@@ -4,9 +4,12 @@ from bs4 import BeautifulSoup
 
 
 class Snowcrows:
+    """Interact with snowcrows.com to get Guild Wars 2 build information."""
+
     def __init__(self) -> None:
-        self.base_url = "https://snowcrows.com/builds"
-        self.headers = {
+        """Initialize an instance of the Snowcrows class."""
+        self._BASE_URL = "https://snowcrows.com/builds"
+        self._HEADERS = {
             "User-Agent": (
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) "
                 "Gecko/20100101 Firefox/118.0"
@@ -15,31 +18,34 @@ class Snowcrows:
         self._clear_cache()
 
     def _clear_cache(self) -> None:
-        # Clear the cache for methods that need to be invalidated
+        """Clear the cache for all decorated methods."""
         self.get_build_names.cache_clear()
         self.get_build.cache_clear()
 
     def _get_html_content(self, url: str) -> BeautifulSoup:
-        # Request website and parse content
-        website = requests.get(url, headers=self.headers)
+        """Request website content and parse it into a BeautifulSoup object."""
+        website = requests.get(url, headers=self._HEADERS)
         return BeautifulSoup(website.content, "html.parser")
 
     def _parse_build(self) -> None:
         # To be done
         pass
 
+    def _parse_equipment(self) -> None:
+        # To be done
+        pass
+
     @functools.lru_cache(maxsize=None)
     def get_build_names(self, profession_name: str) -> list[str]:
+        """Get a list of build names for the specified profession name."""
         build_names = []
         for category in ["recommended", "effective"]:
-            # Prepare request for the category
             url = (
-                f"{self.base_url}"
+                f"{self._BASE_URL}"
                 f"?profession={profession_name}&category={category}"
             )
-            # Get html content for the url
             html_content = self._get_html_content(url)
-            # Find all <h2> elements that do not contain "Related guides"
+            # Extract text from <h2> elements, excluding "Related guides"
             h2_elements = [
                 element.text
                 for element in html_content.find_all("h2")
@@ -51,20 +57,19 @@ class Snowcrows:
 
     @functools.lru_cache(maxsize=None)
     def get_build(self, profession_name: str, build_name: str):
+        """Get detailed information for a specific build name."""
         build_name = build_name.lower().replace(" ", "-")
-        # Prepare request
         url = (
-            f"{self.base_url}"
+            f"{self._BASE_URL}"
             f"/{profession_name}/{build_name}"
         )
-        # Get html content for the url
         html_content = self._get_html_content(url)
-        # To be continued...
+        # To be done
 
 
 if __name__ == "__main__":
     snowcrows = Snowcrows()
-    profession_name = "Thief"
+    profession_name = "PROFESSION-NAME"
     build_names = snowcrows.get_build_names(profession_name)
     print(f"Available builds for {profession_name}:")
     for name in build_names:
