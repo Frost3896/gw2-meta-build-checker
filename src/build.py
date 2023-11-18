dfefbdc4bcc07d6336c2e495239fdb609fd4fff0
Frost3896 <146024772+Frost3896@users.noreply.github.com>
2023-11-18 20:38:09 +0100

    Add empty() class method

--- src/build.py
+++ src/build.py
@@ -1,5 +1,9 @@
 from dataclasses import dataclass
 
+# Define an empty ID and name.
+_EMPTY_ID = 0
+_EMPTY_NAME = ""
+
 
 @dataclass
 class Skill:
@@ -7,12 +11,28 @@ class Skill:
     id: int
     name: str
 
+    @classmethod
+    def empty(cls):
+        """Create an instance with empty values."""
+        return cls(
+            id=_EMPTY_ID,
+            name=_EMPTY_NAME
+        )
+
 
 @dataclass
 class Trait:
     """Represent a trait with an ID and name."""
     id: int
     name: str
+
+    @classmethod
+    def empty(cls):
+        """Create an instance with empty values."""
+        return cls(
+            id=_EMPTY_ID,
+            name=_EMPTY_NAME
+        )
 
 
 @dataclass
@@ -22,6 +42,15 @@ class Specialization:
     name: str
     traits: list[Trait]
 
+    @classmethod
+    def empty(cls):
+        """Create an instance with empty values."""
+        return cls(
+            id=_EMPTY_ID,
+            name=_EMPTY_NAME,
+            traits=[Trait.empty()] * 3
+        )
+
 
 @dataclass
 class Build:
@@ -29,3 +58,12 @@ class Build:
     name: str
     skills: list[Skill]
     specializations: list[Specialization]
+
+    @classmethod
+    def empty(cls):
+        """Create an instance with empty values."""
+        return cls(
+            name=_EMPTY_NAME,
+            skills=[Skill.empty()] * 5,
+            specializations=[Specialization.empty()] * 3
+        )
--- src/equipment.py
+++ src/equipment.py
@@ -1,11 +1,24 @@
 from dataclasses import dataclass
 
+# Define an empty ID, name and slot.
+_EMPTY_ID = 0
+_EMPTY_NAME = ""
+_EMPTY_SLOT = ""
+
 
 @dataclass
 class Stats:
     """Represent stats with an ID and name."""
     id: int
     name: str
+
+    @classmethod
+    def empty(cls):
+        """Create an instance with empty values."""
+        return cls(
+            id=_EMPTY_ID,
+            name=_EMPTY_NAME
+        )
 
 
 @dataclass
@@ -14,6 +27,14 @@ class Upgrade:
     id: int
     name: str
 
+    @classmethod
+    def empty(cls):
+        """Create an instance with empty values."""
+        return cls(
+            id=_EMPTY_ID,
+            name=_EMPTY_NAME
+        )
+
 
 @dataclass
 class Infusion:
@@ -21,12 +42,28 @@ class Infusion:
     id: int
     name: str
 
+    @classmethod
+    def empty(cls):
+        """Create an instance with empty values."""
+        return cls(
+            id=_EMPTY_ID,
+            name=_EMPTY_NAME
+        )
+
 
 @dataclass
 class Relic:
     """Represent a relic with an ID and name."""
     id: int
     name: str
+
+    @classmethod
+    def empty(cls):
+        """Create an instance with empty values."""
+        return cls(
+            id=_EMPTY_ID,
+            name=_EMPTY_NAME
+        )
 
 
 @dataclass
@@ -37,6 +74,16 @@ class Armor:
     upgrade: Upgrade
     infusion: Infusion
 
+    @classmethod
+    def empty(cls):
+        """Create an instance with empty values."""
+        return cls(
+            slot=_EMPTY_SLOT,
+            stats=Stats.empty(),
+            upgrade=Upgrade.empty(),
+            infusion=Infusion.empty()
+        )
+
 
 @dataclass
 class Weapon:
@@ -46,6 +93,16 @@ class Weapon:
     upgrades: list[Upgrade]
     infusions: list[Infusion]
 
+    @classmethod
+    def empty(cls):
+        """Create an instance with empty values."""
+        return cls(
+            slot=_EMPTY_SLOT,
+            stats=Stats.empty(),
+            upgrades=[Upgrade.empty()] * 2,
+            infusions=[Infusion.empty()] * 2
+        )
+
 
 @dataclass
 class Accessory:
@@ -54,12 +111,32 @@ class Accessory:
     stats: Stats
     infusions: list[Infusion]
 
+    @classmethod
+    def empty(cls):
+        """Create an instance with empty values."""
+        return cls(
+            slot=_EMPTY_SLOT,
+            stats=Stats.empty(),
+            infusions=[Infusion.empty()] * 3
+        )
+
 
 @dataclass
 class Equipment:
-    """Represent an equipment with a name, armors, accessories and weapons."""
+    """Represent an equipment with a name, armors, weapons and accessories."""
     name: str
     armors: list[Armor]
     weapons: list[Weapon]
     accessories: list[Accessory]
     relic: Relic
+
+    @classmethod
+    def empty(cls):
+        """Create an instance with empty values."""
+        return cls(
+            name=_EMPTY_NAME,
+            armors=[Armor.empty()] * 6,
+            weapons=[Weapon.empty()] * 4,
+            accessories=[Accessory.empty()] * 6,
+            relic=Relic.empty()
+        )
