60b3e070fa0f850a40f7e15746868eb3289b541a
Frost3896 <146024772+Frost3896@users.noreply.github.com>
2024-01-01 15:51:23 +0100

    Initial commit

---
+++ src/constants.py
@@ -0,0 +1,49 @@
+# Define an empty ID, name, slot and type.
+EMPTY_ID = 0
+EMPTY_NAME = ""
+EMPTY_SLOT = ""
+EMPTY_TYPE = ""
+
+# Define the desired order for armor, weapon, and accessory slots.
+ARMOR_SLOTS = (
+    "Helm",
+    "Shoulders",
+    "Coat",
+    "Gloves",
+    "Leggings",
+    "Boots"
+)
+WEAPON_SLOTS = (
+    "WeaponA1",
+    "WeaponA2",
+    "WeaponB1",
+    "WeaponB2"
+)
+ACCESSORY_SLOTS = (
+    "Backpack",
+    "Accessory1",
+    "Accessory2",
+    "Amulet",
+    "Ring1",
+    "Ring2"
+)
+
+# Define the possible weapon types.
+WEAPON_TYPES = (
+    "Axe",
+    "Dagger",
+    "Mace",
+    "Pistol",
+    "Sword",
+    "Scepter",
+    "Focus",
+    "Shield",
+    "Torch",
+    "Warhorn",
+    "Greatsword",
+    "Hammer",
+    "Longbow",
+    "Rifle",
+    "ShortBow",
+    "Staff"
+)
